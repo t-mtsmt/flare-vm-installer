1ffc98da5ee4ff40eccd20fecd42111c9cbbec69
Takashi Matsumoto <77025706+t-mtsmt@users.noreply.github.com>
2024-09-17 03:35:41 +0900

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -23,30 +23,28 @@ Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' -Name "PromptOnSecureDesktop" -Value 0
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' -Name "EnableLUA" -Value 0
 
+# Make FLARE VM Installer
+Invoke-WebRequest "https://raw.githubusercontent.com/mandiant/flare-vm/main/install.ps1" -OutFile .\install.ps1
+Unblock-File .\install.ps1
+
+$link = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\flarevm-installer.lnk"
+$powershell_path = '"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"'
+$cmdargs = "-Command `"& {Start powershell -ArgumentList '-ExecutionPolicy Unrestricted \`"$workdir\install.ps1\`"' -Verb RunAs; Remove-Item  -Force `'$link`' -ErrorAction 'ignore'}`""
+
+$res = New-Item $(Split-Path -Path $link -Parent) -ItemType Directory -Force
+$WshShell = New-Object -comObject WScript.Shell
+$shortcut = $WshShell.CreateShortcut($link)
+$shortcut.TargetPath = $powershell_path
+$shortcut.Arguments = $cmdargs
+$shortcut.WorkingDirectory = $workdir
+$shortcut.Save()
+
 # Disable Windows Defender
 Set-MpPreference -DisableRealtimeMonitoring $true -DisableScriptScanning $true -ExclusionPath $scripts
 Start-Sleep -Seconds 3.0
 Invoke-WebRequest "https://github.com/ionuttbara/windows-defender-remover/archive/refs/tags/release_def_12_8.zip" -OutFile .\windows-defender-remover.zip
 Expand-Archive -Path .\windows-defender-remover.zip
 $windows_defender_remover_path = $(Resolve-Path .\windows-defender-remover\*)
-$content = $(Get-Content $windows_defender_remover_path\Script_Run.bat) -replace "choice /C:yas /N","goto removedef" -replace "shutdown /r /f /t 0","exit"
+$content = $(Get-Content $windows_defender_remover_path\Script_Run.bat) -replace "choice /C:yas /N","goto removedef"
 Out-File -Encoding utf8 -InputObject $content $windows_defender_remover_path\Script_Run.bat
 &"$windows_defender_remover_path\Script_Run.bat"
-
-# Make FLARE VM Installer
-Invoke-WebRequest "https://raw.githubusercontent.com/mandiant/flare-vm/main/install.ps1" -OutFile .\install.ps1
-Unblock-File .\install.ps1
-
-$link = "$current_dir\flarevm-installer.lnk"
-$powershell_path = '"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"'
-$cmdargs = "-ExecutionPolicy Unrestricted .\install.ps1"
-    
-$res = New-Item $(Split-Path -Path $link -Parent) -ItemType Directory -Force
-$WshShell = New-Object -comObject WScript.Shell
-$shortcut = $WshShell.CreateShortcut($link)
-$shortcut.TargetPath = $powershell_path
-$shortcut.Arguments = $cmdargs
-$shortcut.WorkingDirectory = $scripts
-$shortcut.Save()
-
-Restart-Computer -Confirm
